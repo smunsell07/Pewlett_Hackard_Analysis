ed2c0fe52889aca6714d1bca073b3eac340c42a6
Bluedog11 <sheilamunsell@hotmail.com>
2022-04-12 13:32:23 -0500

    editing schema.sql

--- schema.sql
+++ schema.sql
@@ -23,6 +23,7 @@ FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
@@ -31,6 +32,7 @@ CREATE TABLE salaries(
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	PRIMARY KEY (emp_no)
 );
+
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
